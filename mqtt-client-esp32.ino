95f490f9fb2508767b88cec96c917012b8fbfc02
Gerardo Marx <gmarx_cc@itmorelia.edu.mx>
2022-08-19 18:27:55 -0500

    better code presentation

--- mqtt-client-esp32.ino
+++ mqtt-client-esp32.ino
@@ -11,55 +11,52 @@ const char* mqttPassword = "";
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-void OnMqttReceived(char *topic, byte *payload, unsigned int length)
-{
-    Serial.print("Received on ");
-    Serial.print(topic);
-    Serial.print(": ");
-    String content = "";
-    for (size_t i = 0; i < length; i++)
-    {
-        content.concat((char)payload[i]);
+void OnMqttReceived(char *topic, byte *payload, unsigned int length){
+  Serial.print("Received on ");
+  Serial.print(topic);
+  Serial.print(": ");
+  String content = "";
+  for (size_t i = 0; i < length; i++){
+    content.concat((char)payload[i]);
     }
-    Serial.print(content);
-    Serial.println();
-}
+  Serial.print(content);
+  Serial.println();
+  }
 
-void setup()
-{ Serial.begin(115200);
+void setup(){ 
+  Serial.begin(115200);
   WiFi.begin(ssid, password);
   Serial.println("...................................");
 
   Serial.print("Connecting to WiFi.");
-  while (WiFi.status() != WL_CONNECTED)
-       {  delay(500);
-          Serial.print(".") ;
-       }
+  while (WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+    }
+  Serial.println("");  
   Serial.println("Connected to the WiFi network");
-
+  //mqtt connection
   client.setServer(mqttServer, mqttPort);
-while (!client.connected())
-{      Serial.println("Connecting to MQTT...");
-       if (client.connect("prueba", mqttUser, mqttPassword ))
-           {Serial.println("connected");
-           client.subscribe("data/esp32");
-           client.setCallback(OnMqttReceived);
-           }
-       else
-       {   Serial.print("failed with state ");
-           Serial.print(client.state());
-           delay(2000);
+while (!client.connected()){
+  Serial.println("Connecting to MQTT...");
+  if (client.connect("ESP32", mqttUser, mqttPassword )){
+    Serial.println("connected");
+    client.subscribe("data/esp32");
+    client.setCallback(OnMqttReceived);
+    }
+  else{
+    Serial.print("failed with state ");
+    Serial.print(client.state());
+    delay(2000);
        }
-}
+  }
 }
 
 void loop(){  
   client.loop();
   char str[16];
-  //if(Serial.read()>0){
   sprintf(str, "%u", random(100));
   client.publish("data/esp32", str);
   Serial.println(str);
-  delay(1000);
-  //}   
+  delay(2000);
 }
